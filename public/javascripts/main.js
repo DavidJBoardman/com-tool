0aba472ea98933b0307f5d175d3fb87323fa24fd
Didlid <did.boardman@gmail.com>
2016-11-10 12:38:58 +0000

    Added system for choosing phrases

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,38 @@
+$(document).ready(function() {
+  $('select').material_select();
+});
+
 function submit(){
 
    var title = $('#newtitle').val();
    var description = $('#newdescription').val();
- 
+
    //$.post(url, data, callback)
    $.post('/insert', {title: title, description: description}, function(){
      location.reload(true);
    });
+}
+
+function deleteitem(id){
+   //$.post(url, data, callback)
+   $.post('/delete', {id: id}, function(){
+     location.reload(true);
+   });
+}
+
+
+function update(id, done){
+   var checkboxes = $('input[type=checkbox]');
+   for (var i=0; i<checkboxes.length; i++){
+     checkboxes[i].setAttribute("disabled", "disabled");
+   }
+   //$.post(url, data, callback)
+   $.post('/update', {id: id, done: !done}, function(){
+     location.reload(true);
+   });
+}
+
+//Update dropdown box name
+function txtUpdate(id){
+  $('')
 }
--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
       results.push(row);
     })
     .on('end', function(){
-      res.render('index', { list: results, title: 'Todo list' });
+      res.render('index', { list: results, title: 'Communication Tool' });
     });
 });
 
@@ -32,6 +32,30 @@ router.post('/insert', function(req, res) {
         res.send('success');
       });
   });
+});
+
+//delete a todo item
+router.post('/delete', function(req, res) {
+   pg.connect(process.env.DATABASE_URL, function(err, client) {
+      if (err) throw err;
+      client
+        .query('DELETE FROM todo WHERE pk_id='+req.body.id)
+        .on('end', function(){
+          res.send('success');
+        });
+   });
+});
+
+//update an item
+router.post('/update', function(req, res) {
+   pg.connect(process.env.DATABASE_URL, function(err, client) {
+      if (err) throw err;
+      client
+        .query('UPDATE todo SET done='+req.body.done+' WHERE pk_id='+req.body.id)
+        .on('end', function(){
+          res.send('success');
+        });
+    });
 });
 
 module.exports = router;
